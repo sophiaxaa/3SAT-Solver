8f49be45207eff4edcd59db637fb315e0c7fa805
sophiaxaa <sophialeabo@gmail.com>
2024-11-24 22:01:39 -0500

    added formula generator

--- src/formula_gen.py
+++ src/formula_gen.py
@@ -20,9 +20,23 @@ def generate_3cnf_formula(n,m):
     for i in variables:
         for j in variables:
             for k in variables: 
-                if len({i, j, k} == 3):
+                if len({i, j, k}) == 3:
                     for signs in [(1, 1, 1), (1, 1, -1), (1, -1, 1), (1, -1, -1), 
                                   (-1, 1, 1), (-1, 1, -1), (-1, -1, 1), (-1, -1, -1)]:
                         clause_pool.append((i * signs[0], j * signs[1], k * signs[2]))
 
-    #randomly select m clauses  
+    #randomly select m clauses 
+    if m > len(clause_pool):
+         raise ValueError("requested more clauses than possible combinations.")
+    formula = random.sample(clause_pool, m)
+
+    return formula
+
+if __name__ == "__main__":
+    #test usage
+    n = 5
+    m = 10
+    formula = generate_3cnf_formula(n,m)
+    print("generated 3CNF formula: ")
+    for clause in formula:
+        print(f"({clause[0]} ∨ {clause[1]} ∨ {clause[2]})")
